0479134e4d21b6ef2bd9c74667adedfd2a74e7ce
ArcDrive <46378884+ArcDrive@users.noreply.github.com>
2019-01-16 14:41:55 -0500

    Update main.js
    Updated to control creeps

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 module.exports.loop = function() {
   for (let room in Game.rooms) {
-    var roomName = Game.rooms[room.name]
+    var roomName = Game.rooms[room].name
     var defcon = require('defcon').run(roomName)
     require('room').run(roomName, defcon)
+  }
+
+  for (let name in Game.creeps) {
+    var creep = Game.creeps[name];
+    require('creep').run(creep)
   }
 }
