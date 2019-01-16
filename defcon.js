9fce9253c1db0c80ffc4c6d6d35fbc539895220f
ArcDrive <46378884+ArcDrive@users.noreply.github.com>
2019-01-16 14:31:09 -0500

    Update defcon.js
    first complete functioning version of the Defcon system

--- defcon.js
+++ defcon.js
@@ -1,10 +1,16 @@
 module.exports = {
-  run: function (roomName) {
+  run: function(roomName) {
 
     var defcon = Game.rooms[roomName].memory.defcon
     var hostileCreeps = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS).length
     var cooldDownTicks = Game.rooms[roomName].memory.cooldDownTicks
     var heatUpTicks = Game.rooms[roomName].memory.heatUpTicks
+    var wall = Game.rooms[roomName].find(FIND_STRUCTURES, {
+      filter: {
+        structureType: STRUCTURE_WALL
+      }
+    })
+    var lowestWall = _.min(wall, (s) => s.hits)
 
     if (hostileCreeps == 0 && cooldDownTicks != 0) {
       cooldDownTicks = cooldDownTicks - 1
@@ -20,23 +26,27 @@ module.exports = {
     if (defcon < hostileCreeps) {
       defcon = hostileCreeps
     }
-    if (defcon < heatUpTicks/10) {
+    if (defcon < heatUpTicks / 10 && defcon < 5) {
       defcon++
     }
     if (defcon != 0 && heatUpTicks == 0) {
       defcon = 0
+    }
+    if (defcon > 0 && lowestWall.hits < 1000) {
+      defcon = 5
     }
 
 
     Game.rooms[roomName].memory.defcon = defcon
     Game.rooms[roomName].memory.cooldDownTicks = cooldDownTicks
     Game.rooms[roomName].memory.heatUpTicks = heatUpTicks
+    return defcon
   }
 }
 
 //defcon 0 = normal activity, one ranger
-//defcon 1 = turret gets more energy, one ranger, outside harvesting shut off
-//defcon 2
-//defcon 3
-//defcon 4
-//defcon 5
+//defcon 1 = turret gets more energy, one ranger, outside harvesting shut off, builders switch to repairers
+//defcon 2 = turret gets more energy, one ranger, outside harvesting shut off, upgraders and builders switch to repairers
+//defcon 3 = turret gets more energy, two rangers, outside harvesting shut off, upgraders and builders switch to repairers, call for energy assistance
+//defcon 4 = turret gets more energy, two rangers, call for troop assistance, outside harvesting shut off, upgraders and builders switch to repairers, call for energy assistance
+//defcon 5 = turret gets more energy, two rangers, call for troop assistance, outside harvesting shut off, upgraders and builders switch to repairers, call for energy assistance, safe mode
