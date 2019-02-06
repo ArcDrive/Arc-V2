34ea137ec7243d46931fa98e0b4443fb2f193b07
ArcDrive <46378884+ArcDrive@users.noreply.github.com>
2019-02-06 11:14:33 -0500

    Update main.js
    Updated the room code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 module.exports.loop = function() {
-  for (let room in Game.rooms) {
-    var roomName = Game.rooms[room].name
-    var defcon = require('defcon').run(roomName)
-    require('room').run(roomName, defcon)
+  for (let name in Game.rooms) {
+    var room = Game.rooms[name]
+    var defcon = require('defcon').run(room)
+    require('room').run(room, defcon)
   }
 
   for (let name in Game.creeps) {
