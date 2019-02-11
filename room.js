f07b2488ea8a0092da08e6a32d9ffcc6815fb8eb
ArcDrive <46378884+ArcDrive@users.noreply.github.com>
2019-02-11 10:43:46 -0500

    Update room.js
    added source memory

--- room.js
+++ room.js
@@ -14,5 +14,11 @@ module.exports = {
     for (let spawn of spawns) {
       require('control.spawn').run(spawn)
     }
+    
+    var sources = room.find(FIND_SOURCES)
+    room.memory.source0 = _.sum(room.find(FIND_MY_CREEPS, (c) => c.memory.source == sources[0].id))
+    room.memory.source1 = _.sum(room.find(FIND_MY_CREEPS, (c) => c.memory.source == sources[1].id))
+    room.memory.source2 = _.sum(room.find(FIND_MY_CREEPS, (c) => c.memory.source == sources[2].id))
+    room.memory.source3 = _.sum(room.find(FIND_MY_CREEPS, (c) => c.memory.source == sources[3].id))
   }
 }
